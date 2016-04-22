1bb397e40a9f453748547aeb2007726bcff85755
nw <nw@linecorp.com>
2016-04-22 17:33:46 +0900

    moduleに切り出してみる

--- main.rb
+++ main.rb
@@ -4,6 +4,7 @@ require 'sinatra/reloader'
 require 'json'
 require 'faraday'
 require 'dotenv'
+require './lib/line/bot/client'
 
 class MainApp < Sinatra::Base
 
@@ -16,38 +17,10 @@ class MainApp < Sinatra::Base
     params = JSON.parse request.body.read
     result = params['result']
     result.each{|message|
-      send_message(message['content']['from'], message['content']['text'])
+      Line::Bot::Client.send_message(message['content']['from'], message['content']['text'])
     }
     'done'
   end
 
-  def send_message(to, text)
-    conn = Faraday.new(:url => 'https://trialbot-api.line.me') do |builder|
-      builder.request  :url_encoded
-      builder.response :logger
-      builder.adapter  :net_http
-    end
-      
-    conn.post do |req|
-      req.url '/v1/events'
-      req.headers = {
-        'Content-Type' => 'application/json; charser=UTF-8',
-        'X-Line-ChannelID' => ENV['LINE_CHANNEL_ID'],
-        'X-Line-ChannelSecret' => ENV['LINE_CHANNEL_SECRET'],
-        'X-Line-Trusted-User-With-ACL' => ENV['LINE_CHANNEL_MID']
-      }
-      req.body = {
-        'to' => [to],
-        'toChannel' => "1383378250",
-        'eventType' => "138311608800106203",
-        'content' => {
-          'contentType' => 1,
-          'toType' => 1,
-          'text' =>  text
-        }
-      }.to_json
-    end
-  end
 end
 
-#MainApp.run! :host => 'localhost', :port => 8090
