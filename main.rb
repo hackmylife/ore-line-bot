eb34a799ae7ba6d9296a59f9100d4d2bc01c65fa
nw <nw@linecorp.com>
2016-04-25 15:26:56 +0900

    responseを取り方をちょっと変更

--- main.rb
+++ main.rb
@@ -35,8 +35,7 @@ class MainApp < Sinatra::Base
   def user_check(message)
     mid = message['content']['from']
     unless User.exists?(mid: mid) then
-      result = JSON.parse @@client.get_profile(mid).body
-      profile = result.fetch('contacts').first
+      profile = JSON.parse @@client.get_profile(mid).fetch('contacts').first
       user = User.new
       user.mid = mid
       user.display_name = profile['displayName']
