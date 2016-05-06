a26f61c2f385ac03d59119cb0e9a146127c06a25
nw <nw@linecorp.com>
2016-05-06 19:06:47 +0900

    +

--- lib/parser/datetime.rb
+++ lib/parser/datetime.rb
@@ -36,12 +36,12 @@ module Parser
     def parse_regex(time_srt)
       # min later expression
       if time_srt.match(/([0-9０-９]+)\s*(?:分|min)/) then
-        return Time.now - $1.to_i * 60
+        return Time.now + $1.to_i * 60
       end
 
       # houre later expression
       if time_srt.match(/([0-9０-９]+)\s*(?:時|hour)/) then
-        return Time.now - $1.to_i * 3600
+        return Time.now + $1.to_i * 3600
       end
 
     end
